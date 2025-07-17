92f04762f5b71b80d44f68fc714cbccd84b4bbef
rzkinr <rizkynurrokhim18@gmail.com>
2025-07-17 10:14:39 +0700

    Shuffling answer & addign quiz logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,43 @@
 import { useState } from "react";
-import Qeustions from "../questions.js";
+import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active question</p>;
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  function handleAnswer(answer) {
+    setUserAnswers((prevUserAnswer) => {
+      return [...prevUserAnswer, answer];
+    });
+  }
+
+  if (quizIsComplete) {
+    return (
+      <div id='summary'>
+        <img src={quizCompleteImg} alt='Quiz Completed' />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {shuffleAnswers.map((answer) => (
+            <li key={answer} className='answer'>
+              <button onClick={() => handleAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
